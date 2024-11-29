af53cfdfa111e33a9f85c8a53e24325c8ef5276b
tanvir ahmed <tanvir.073.ahmed@gmail.com>
2024-11-29 17:47:02 +0600

    add logo loader for initial app load

---
+++ src/components/Loaders/LogoLoader.tsx
@@ -0,0 +1,16 @@
+import "../../app/globals.css";
+export default function LogoLoader() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <div className="flex text-red-600 font-bold text-9xl">
+        <span className="letter l">L</span>
+        <span className="letter i">I</span>
+        <span className="letter n">N</span>
+        <span className="letter k">K</span>
+        <span className="letter i">I</span>
+        <span className="letter f">F</span>
+        <span className="letter y">Y</span>
+      </div>
+    </div>
+  );
+}
